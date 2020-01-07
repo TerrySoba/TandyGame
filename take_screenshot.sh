546b70ef3c40b3163ff89f9c072a45d7a58799c4
Torsten Stremlau <torsten@stremlau.eu>
2020-01-07 23:03:35 +0100

    Scale screenshot to 4:3 aspect ratio

--- take_screenshot.sh
+++ take_screenshot.sh
@@ -8,7 +8,10 @@ pid=$!
 sleep 2
 
 # now take a screenshot
-import -window root screenshot.png
+import -window root screenshot_unscaled.png
+
+# now scale to correct aspect ratio
+convert screenshot_unscaled.png -resize 640x480! screenshot.png
 
 # now shut down dosemu
 kill $pid
