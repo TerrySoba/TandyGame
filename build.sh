ddcfbec68be6b072a5e10a106d571d7dc5334f31
Torsten Stremlau <torsten@stremlau.eu>
2020-01-16 23:21:04 +0100

    Add readme.txt

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ docker run --user $(id -u):$(id -g) -v `pwd`/source/:/build open_watcom &&
 rm -rf release && mkdir release &&
 cat source/distfiles.txt | xargs -I FILENAME cp source/FILENAME release
 
+# append git hash to readme.txt
+echo `git rev-parse HEAD` >> release/readme.txt
+
